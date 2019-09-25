e235ca54fcded4e16a24aaf5582a86c7b1121d3d
namelessbliss <anonimo2911.2012@gmail.com>
2019-09-24 19:25:12 -0500

    StreetViewPanorama

--- app/src/main/java/com/app/nb/androidmapas/MapsActivityApiKey.java
+++ app/src/main/java/com/app/nb/androidmapas/MapsActivityApiKey.java
@@ -1,49 +1,34 @@
 package com.app.nb.androidmapas;
 
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivityApiKey extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivityApiKey extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
 
-    private GoogleMap mMap;
+    private StreetViewPanorama streetViewMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_api_key);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        streetViewMap = streetViewPanorama;
+        streetViewMap.setPosition(new LatLng(-12.04318, -77.02824));
     }
 }
